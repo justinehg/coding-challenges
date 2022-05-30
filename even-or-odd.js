b1b17f08a6c26f475da1e9a3032a6b06b194eb23
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-05-30 01:54:27 -0500

    Add first solution
    used conditionals to determine whether the number is even or odd and return the result

---
+++ even-or-odd.js
@@ -0,0 +1,12 @@
+// 1. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function evenOrOdd(numbers){
+    if(number % 2 == 0){
+        return "Even";
+    }else{
+        return "Odd";
+    }
+}
+    // OR
+function evenOrOdd(numbers){
+    return numbers % 2 ? "Even" : "Odd";
+}
