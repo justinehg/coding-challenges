3ecf6102f68fba8e48c759869780ce6e806ff6b7
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-06-02 23:37:14 -0500

    add first solution
    created a function that determines whether the program should multiply the given number by 8 or 9. Utilized the concept of conditionals to have the number multiplied by 8 if even and 9 if odd.

---
+++ CodeWars/8-kyu/multiply-even-or-odd.js
@@ -0,0 +1,12 @@
+// 5. This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+function multiplyEvenOrOdd(number){
+    if(number % 2 == 0){
+        return number * 8;
+    }else{
+        return number * 9;
+    }
+}
+    // OR
+function multiplyEvenOrOdd(number){
+    return number % 2 ? number * 8 : number * 9
+}
