3daaea46ae94237ca9f4eb8a792bbb94857e37a1
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-06-13 23:24:12 -0500

    add first solution

---
+++ CodeWars/8-kyu/correct-misinpretated-texts.js
@@ -0,0 +1,10 @@
+// 14.  correct the errors in the digitised text. You only have to handle the following mistakes:
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+function correct(string){
+    return string.replace(/0/g, "O")
+                 .replace(/1/g, "I")
+                 .replace(/5/g, "S");
+}
