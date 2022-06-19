652dda2203aa28bdbaf41c78109c3eb35f52ac94
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-06-18 23:54:55 -0500

    delete duplicate file

--- find-the-needle.js
+++
@@ -1,4 +0,0 @@
-// 19. Write a function findNeedle() that takes an array full of junk but containing one "needle". After your function finds the needle it should return a message (as a string) that says: "found the needle at position " plus the index it found the needle.
-function findNeedle(haystack) {
-    return "found the needle at position " + haystack.indexOf("needle");
-  }
