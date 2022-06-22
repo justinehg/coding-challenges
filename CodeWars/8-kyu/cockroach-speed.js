6a809c4297301e210d7d6347f6a3243b6f2b6214
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-06-21 23:24:56 -0500

    add first solution
    created a function that converts the speed of a cockroach from km/hr to cm/sec, using the conversion formula and Math.floor() to round its result

---
+++ CodeWars/8-kyu/cockroach-speed.js
@@ -0,0 +1,7 @@
+// 22. The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+// Note: 0.036 km/h = 1 cm/s & Math.floor() rounds down the result to whole number
+
+function cockroachSpeed(s) {
+    return Math.floor(s / 0.036)
+  }
