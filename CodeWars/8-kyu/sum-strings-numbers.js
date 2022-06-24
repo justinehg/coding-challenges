a915357e0140ec159dae234b544248bdde0827b7
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-06-23 23:09:23 -0500

    add first solution

---
+++ CodeWars/8-kyu/sum-strings-numbers.js
@@ -0,0 +1,5 @@
+// 24. Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers. Return your answer as a number.
+// Note: .reduce() is to return the sum of all the elements in an array
+function sumMix(x){
+    return x.reduce((x, y) => Number(x) + Number(y), 0);
+  }
