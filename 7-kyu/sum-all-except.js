64aa4234ded61133627a13aafe1cbfb53c46cadf
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-07-01 23:12:53 -0500

    add first solution

---
+++ 7-kyu/sum-all-except.js
@@ -0,0 +1,9 @@
+// 27. Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ). The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value. Mind the input validation.
+function sumArray(array)  {
+    if(array && array.length > 1) {
+        const sortedArray = array.sort((a,b) => a - b).slice(1, -1)
+        return sortedArray.reduce((acc, cur) => acc + cur, 0)
+    }
+
+    return 0
+}
