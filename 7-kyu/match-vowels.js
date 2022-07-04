5e656675796d4b6dbe92b35889e6b717d4807984
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-07-04 15:53:39 -0500

    add first solution

---
+++ 7-kyu/match-vowels.js
@@ -0,0 +1,12 @@
+// Return the number (count) of vowels in the given string. We will consider a, e, i, o, u as vowels for this Kata (but not y). The input string will only consist of lower case letters and/or spaces.
+function getCount(str) {
+    let vowelsCount = 0
+    const vowels = ["a", "e", "i", "o", "u"]
+    for(let char of str) {
+        if(vowels.includes(char)) {
+            vowelsCount++
+        }
+    }
+  
+    return vowelsCount;
+  }
