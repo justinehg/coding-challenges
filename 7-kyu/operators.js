fa463a88e709d1d333756dcea403fb89503473c9
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-07-18 23:37:44 -0500

    add updated solution
    used switch() for a shorter and more efficient solution

--- 7-kyu/operators.js
+++ 7-kyu/operators.js
@@ -6,3 +6,16 @@ function arithmetic(a, b, operator) {
     if (operator === 'multiply') return a * b;
     if (operator === 'divide') return a / b;
   }
+// BETTER SOLUTION: 
+function arithmetic(a, b, operator){
+    switch(operator) {
+      case 'add':
+        return a + b;
+      case 'subtract':
+        return a - b;
+      case 'multiply':
+        return a * b;
+      case 'divide':
+        return a / b;
+    }
+  }
