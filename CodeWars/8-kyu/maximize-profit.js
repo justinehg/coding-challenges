9923cf35d2d9a95e83cb99571699b117a66e4872
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-09-03 23:55:57 +0700

    update file

--- CodeWars/8-kyu/maximize-profit.js
+++ CodeWars/8-kyu/maximize-profit.js
@@ -1,4 +1,4 @@
-// 26. Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest. Write a function that returns both the minimum and maximum number of the given list/array.
+// Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest. Write a function that returns both the minimum and maximum number of the given list/array.
 function minMax(arr){
     return [Math.min(...arr), Math.max(...arr)];
   }
