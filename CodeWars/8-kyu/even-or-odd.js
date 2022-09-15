41d8cf8e9309a12ffc79608ba2a28df27c7061fe
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-09-14 22:03:43 -0500

    add updated solution

--- CodeWars/8-kyu/even-or-odd.js
+++ CodeWars/8-kyu/even-or-odd.js
@@ -1,4 +1,4 @@
-// 1. Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 function evenOrOdd(numbers){
     if(number % 2 == 0){
         return "Even";
