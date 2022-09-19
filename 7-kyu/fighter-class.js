a17c4dc9a60a7eebaff63f679cd352a2808e005f
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-09-18 23:21:45 -0500

    add first solution

---
+++ 7-kyu/fighter-class.js
@@ -0,0 +1,12 @@
+// Create a function that returns the name of the winner in a fight between two fighters.Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0. Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language. Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    var fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
+    var fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
+    if(fac1 < fac2) {
+      return fighter2.name;
+    } else if(fac2 < fac1) {
+      return fighter1.name;
+    } else {
+      return firstAttacker;
+    }
+  }
