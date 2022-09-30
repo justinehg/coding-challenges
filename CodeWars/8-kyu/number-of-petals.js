a28f8de2622421a98d6570d7de848d3cea6f5d70
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-09-29 23:09:02 -0500

    add updated solution

--- CodeWars/8-kyu/number-of-petals.js
+++ CodeWars/8-kyu/number-of-petals.js
@@ -1,4 +1,4 @@
-// 7. Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
 function lovePetals(flower1, flower2){
     return flower1 % 2 != flower2 % 2;
 }
