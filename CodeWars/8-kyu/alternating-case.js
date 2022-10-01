76a1594ac667ccd86ab5ba499616d053b8013cb4
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-09-30 23:09:44 -0500

    add updated solution

--- CodeWars/8-kyu/alternating-case.js
+++ CodeWars/8-kyu/alternating-case.js
@@ -1,4 +1,4 @@
-// 13. Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example: altERnaTIng cAsE <=> ALTerNAtiNG CaSe || altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example: altERnaTIng cAsE <=> ALTerNAtiNG CaSe || altERnaTIng cAsE <=> ALTerNAtiNG CaSe
 String.prototype.toAlternatingCase = function () {
     return this.split("").map(a => a === a.toUpperCase() ? a.toLowerCase() : a.toUpperCase()).join("");
   }
