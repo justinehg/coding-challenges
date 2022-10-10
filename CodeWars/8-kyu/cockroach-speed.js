591c16a63f7e0e2ceae3994d9098261cc47c89f8
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-10-09 22:39:00 -0500

    add updated solution

--- CodeWars/8-kyu/cockroach-speed.js
+++ CodeWars/8-kyu/cockroach-speed.js
@@ -1,4 +1,4 @@
-// 22. The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
 
 // Note: 0.036 km/h = 1 cm/s & Math.floor() rounds down the result to whole number
 
