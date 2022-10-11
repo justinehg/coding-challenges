cb004f20c77f30d4a13d6e547130dcb3d6b76e48
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-10-10 22:09:28 -0500

    add updated solution

--- CodeWars/8-kyu/correct-misinpretated-texts.js
+++ CodeWars/8-kyu/correct-misinpretated-texts.js
@@ -1,4 +1,4 @@
-// 14.  correct the errors in the digitised text. You only have to handle the following mistakes:
+// Correct the errors in the digitised text. You only have to handle the following mistakes:
 // S is misinterpreted as 5
 // O is misinterpreted as 0
 // I is misinterpreted as 1
