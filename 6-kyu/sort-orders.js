84cfc745b90f23a1e92e03aeb92fe1a560f2694b
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-10-19 23:15:34 -0500

    add updated solution

--- 6-kyu/sort-orders.js
+++ 6-kyu/sort-orders.js
@@ -1,8 +1,6 @@
 // Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
 
-// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
-
-// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0). If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
 
 function order(words){
     let arr = words.split(' ')
