430b0f902ed1ba623631ca9f5c00971f8580d8e8
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-10-28 22:18:51 -0500

    add first solution

---
+++ 6-kyu/duplicate-encoder.js
@@ -0,0 +1,11 @@
+// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+function duplicateEncode(word){
+    return word
+      .toLowerCase()
+      .split('')
+      .map( function (a, i, w) {
+        return w.indexOf(a) == w.lastIndexOf(a) ? '(' : ')'
+      })
+      .join('');
+    }
+  
