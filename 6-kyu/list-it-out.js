0e84f9b30b336027526a694852893cfc92026656
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-11-17 22:38:10 -0600

    add first solution

--- 6-kyu/list-it-out.js
+++ 6-kyu/list-it-out.js
@@ -12,3 +12,11 @@ const deleteNth = (lst, N) => {
   
     return res;
   }
+// .OR
+function deleteNth(arr,x) {
+  var cache = {};
+  return arr.filter(function(n) {
+    cache[n] = (cache[n]||0) + 1;
+    return cache[n] <= x;
+  });
+}
