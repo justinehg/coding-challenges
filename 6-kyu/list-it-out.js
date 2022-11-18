c3ae65d154134793a7a805d6605a0eaa85c8ded4
Your Name--global user.email justinehoang21@gmail.com <you@example.com>
2022-11-17 22:36:34 -0600

    add first solution

---
+++ 6-kyu/list-it-out.js
@@ -0,0 +1,14 @@
+// Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+const deleteNth = (lst, N) => {
+    const res = [];
+    const countNums = {};
+  
+    lst.forEach((el, idx) => {
+      countNums[el] = countNums[el] ? countNums[el] + 1 : 1;
+      if(countNums[el] <= N) {
+        res.push(el);
+      }
+    })
+  
+    return res;
+  }
