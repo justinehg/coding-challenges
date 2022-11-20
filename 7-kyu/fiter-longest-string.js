76e824ebbf78a945456b6e6b00d57d682d1d4e80
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-11-19 21:42:30 -0600

    add updated solution

--- 7-kyu/fiter-longest-string.js
+++ 7-kyu/fiter-longest-string.js
@@ -1,11 +1,4 @@
-// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
-// Example:
-// a = "xyaabbbccccdefww"
-// b = "xxxxyyyyabklmopq"
-// longest(a, b) -> "abcdefklmopqwxy"
-
-// a = "abcdefghijklmnopqrstuvwxyz"
-// longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or 
 function longest(s1, s2) {
     return (s1 + s2).split('').filter(onlyUnique).sort().join('')
   }
