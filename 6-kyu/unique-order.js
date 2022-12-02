cdac6e478d02ec2bf71080b0ee651cf14b685750
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-12-01 22:55:52 -0600

    add updated solution

--- 6-kyu/unique-order.js
+++ 6-kyu/unique-order.js
@@ -9,3 +9,16 @@ var uniqueInOrder=function(iterable){
     
     return result
   }
+// .OR
+function uniqueInOrder(it) {
+  var result = []
+  var last
+  
+  for (var i = 0; i < it.length; i++) {
+    if (it[i] !== last) {
+      result.push(last = it[i])
+    }
+  }
+  
+  return result
+}
