2a2be7ca9bf80342c8a9012a21e60d14eac688ea
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-12-01 23:18:36 -0600

    add first solution

--- 6-kyu/calc-paperwork.js
+++ 6-kyu/calc-paperwork.js
@@ -1,4 +1,4 @@
-// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages
 function paperwork(n, m) {
     if( n < 0 || m < 0 ) {
         return 0 
