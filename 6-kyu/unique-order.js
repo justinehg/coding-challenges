7ec182584312c1dabeb0fb39246a0d8d43ecd7c8
Justine Hoang <you@example.com>
2022-12-01 22:54:41 -0600

    add first solution

---
+++ 6-kyu/unique-order.js
@@ -0,0 +1,11 @@
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+var uniqueInOrder=function(iterable){
+    const result = []
+    for(let i = 0; i < iterable.length; i++){
+      if(iterable[i] !== iterable[i + 1]){
+        result.push(iterable[i])
+      }
+    }
+    
+    return result
+  }
