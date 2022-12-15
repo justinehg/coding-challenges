ced6070c7101367b5f264b2c9a8853a749fabc2a
Justine Hoang <you@example.com>
2022-12-14 22:06:22 -0600

    add first solution

---
+++ sum-the-square.js
@@ -0,0 +1,13 @@
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+function squareSum(numbers) {
+    let sum = 0
+    for( let i = 0; i < numbers.length; i++) {
+        sum += numbers[i] **2
+    }
+    return sum
+}
+
+// MORE EFFICIENT SOLUTION
+function squareSum(numbers){
+    return numbers.reduce((sum,num) => sum + (num * num), 0);
+  }
