d46224c54f066ba2ec7368538d7b6850aecf2248
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2022-12-21 22:48:19 -0600

    add first solution

--- 6-kyu/display-texts.js
+++ 6-kyu/display-texts.js
@@ -12,12 +12,12 @@ function likes(names) {
   }
 
 //   MORE EFFICIENT SOLUTION
-function likes(names) {
-    return {
-      0: 'no one likes this',
-      1: `${names[0]} likes this`, 
-      2: `${names[0]} and ${names[1]} like this`, 
-      3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
-      4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
-    }[Math.min(4, names.length)]
-  }
+// function likes(names) {
+//     return {
+//       0: 'no one likes this',
+//       1: `${names[0]} likes this`, 
+//       2: `${names[0]} and ${names[1]} like this`, 
+//       3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
+//       4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
+//     }[Math.min(4, names.length)]
+//   }
