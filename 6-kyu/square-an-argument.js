fa6121b799f57f33817ab01be8924f44759ad612
Justine Hoang <you@example.com>
2022-12-28 23:14:42 -0600

    add first solution

---
+++ 6-kyu/square-an-argument.js
@@ -0,0 +1,8 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+function square(n) {
+    return n*n;
+}
+
+// OR
+
+const square = (n) => n * n;
---
+++ 6-kyu/vowel-remover.js
@@ -0,0 +1,9 @@
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+function shortcut(string){
+    return string.replace(/[aeiou]+/g, "");
+  }
+
+// .OR
+function shortcut(string){
+    return string.replace(/[aeiou]/g,'')
+  }
