f5ebf0eca8a9db79adc52abbab8f01e3e8066265
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-01-10 23:55:26 -0600

    add updated solution

--- 6-kyu/two-sum.js
+++ 6-kyu/two-sum.js
@@ -7,12 +7,3 @@ function twoSum(numbers, target) {
       }
     }
   }
-
-// OR
-function twoSum(numbers, target) {
-    for (var i = 0; i < numbers.length-1; i++) {
-        for (var j = i+1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === target) return [i, j];
-        }
-    }
-}
