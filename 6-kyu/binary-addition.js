c6cfb654b4082ae1cdcb9911bd8aa35844664319
Justine Hoang <justinehoang21@gmail.com>
2023-01-21 15:14:51 -0600

    add first solution

---
+++ 6-kyu/binary-addition.js
@@ -0,0 +1,4 @@
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+function addBinary (a,b) {
+    return (a+b).toString(2)
+}
