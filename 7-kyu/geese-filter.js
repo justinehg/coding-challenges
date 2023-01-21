236a9e5a5526683580a547d3bbaa3972e531a2c4
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-01-20 23:49:46 -0600

    add updated solution

--- 7-kyu/geese-filter.js
+++ 7-kyu/geese-filter.js
@@ -1,5 +1,4 @@
 // Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
-// Note: Filter out the string words that match with ones in "geese"
 function gooseFilter (birds) {
     const geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
     
