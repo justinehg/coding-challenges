7d9de11f94021a2c6284eea94dacc18a06d6732e
Justine Hoang <justinehoang21@gmail.com>
2023-01-23 23:13:16 -0600

    add first solution

---
+++ 6-kyu/square-the-digit.js
@@ -0,0 +1,14 @@
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+function squareDigits(num){
+    let newNum = new Array();
+        ('' + num).split('').map(n => {
+            newNum.push(n**2);
+        })
+    
+    return parseInt(newNum.join(''));
+}
+// MORE EFFICIENT SOLUTION
+function squareDigits(num){
+    return Number(('' + num).split('').map(function (val) { return val * val;}).join(''));
+    
+  }
