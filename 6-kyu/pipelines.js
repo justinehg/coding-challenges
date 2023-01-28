b8a3d022acc2ee6dd6f2779f2870ddc54018b6fc
Justine Hoang <justinehoang21@gmail.com>
2023-01-28 15:15:04 -0600

    add first solution

--- 6-kyu/pipelines.js
+++ 6-kyu/pipelines.js
@@ -12,3 +12,16 @@ function fixPipes(numbers) {
 
     return result;
 }
+
+// .OR
+
+function pipeFix(numbers){
+    let first = numbers[0];
+    let last = numbers[numbers.length - 1];
+
+    let arr = [];
+    for (let i = first; i <= last; i++) {
+        arr.push(i);
+    }
+    return arr;
+}
