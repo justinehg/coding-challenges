6e5af7c0aa66be8766a876703c648994ced5d11c
Justine Hoang <justinehoang21@gmail.com>
2023-01-28 15:54:00 -0600

    add first solution

---
+++ 5-kyu/duplicates-lc.js
@@ -0,0 +1,12 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+let containsDuplicate = function(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if(nums[i] == nums[j]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
