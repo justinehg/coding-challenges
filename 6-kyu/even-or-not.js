ec5c92b29de88e620ab548ffcd8895379b762c08
Justine Hoang <justinehoang21@gmail.com>
2023-01-28 15:08:35 -0600

    add first solution

---
+++ 6-kyu/even-or-not.js
@@ -0,0 +1,6 @@
+// Your code will determine if the number passed is even (or not). The function needs to return either a true or false. 
+// Notes: Numbers may be positive or negative, integers or floats.
+
+function testEven(n) {
+    return (n % 2 == 0) ? true : false;
+}
---
+++ 6-kyu/pipelines.js
@@ -0,0 +1,14 @@
+// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+// Notes: Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+function fixPipes(numbers) {
+    let min = Math.min.apply(this, numbers);
+    let max = Math.max.apply(this, numbers);
+    let result = []
+
+    for (let i = min; i <= max; i++) {
+        result.push(i);
+    }
+
+    return result;
+}
