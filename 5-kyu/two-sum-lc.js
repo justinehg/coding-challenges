78f7d076f0907726c67a6eb29c8e2d8b8289d204
Justine Hoang <justinehoang21@gmail.com>
2023-02-03 01:33:05 -0600

    add first solution

--- 5-kyu/two-sum-lc.js
+++ 5-kyu/two-sum-lc.js
@@ -1,7 +1,7 @@
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // Notes: You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-let twoSum = function (nums, target) {
+function twoSum(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i+1; j < nums.length; j++) {
             if (nums[i] + nums[j] == target) {
