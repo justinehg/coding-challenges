b78246823ba9304b8e41651f4ed073288633c14a
Justine Hoang <justinehoang21@gmail.com>
2023-02-03 01:32:17 -0600

    add first solution

--- 5-kyu/anagram.js
+++ 5-kyu/anagram.js
@@ -1,5 +1,5 @@
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-let isAnagram = function(s,t) {
+function isAnagram(s,t) {
     let str1 = s.split('').sort().join('');
     let str2 = t.split('').sort().join('');
 
