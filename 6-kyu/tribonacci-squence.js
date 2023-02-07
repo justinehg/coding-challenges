7b9991d09ed18f4d8095648cbe98e23ed20dee76
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-02-06 23:09:19 -0600

    add first solution

--- 6-kyu/tribonacci-squence.js
+++ 6-kyu/tribonacci-squence.js
@@ -1,4 +1,4 @@
-// you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+// For this challenge, you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
 function tribonacci(signature,n) {
     var trib = signature;
     for (i = 3; i < n; i++) {
