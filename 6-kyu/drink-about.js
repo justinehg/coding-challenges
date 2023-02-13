915b3d2207a09ed32728b328f62b36419f84d16f
Justine Hoang <justinehoang21@gmail.com>
2023-02-12 18:43:12 -0600

    add first solution

--- 6-kyu/drink-about.js
+++ 6-kyu/drink-about.js
@@ -16,3 +16,9 @@ function peopleWithAgeDrink(old) {
       return ("drink whisky");
       }
   };
+// MORE EFFICIENT SOLUTION
+
+const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
