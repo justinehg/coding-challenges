06cd9379dd5b3c675257d8a30085cabdf2548e33
Justine Hoang <justinehoang21@gmail.com>
2023-02-12 18:42:38 -0600

    add first solution

---
+++ 6-kyu/drink-about.js
@@ -0,0 +1,18 @@
+// Make a function that receive age, and return what they drink.
+// Rules:
+// Children under 14 old.
+// Teens under 18 old.
+// Young under 21 old.
+// Adults have 21 or more.
+
+function peopleWithAgeDrink(old) {
+    if(old < 14){
+      return ("drink toddy");
+      } else if((old < 18) && (old >= 14)){
+      return ("drink coke");
+      } else if((old < 21) && (old >= 18)){
+      return ("drink beer");
+      } else if(old >= 21){
+      return ("drink whisky");
+      }
+  };
