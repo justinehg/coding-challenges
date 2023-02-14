8ebb65314b9e90a421035b0f8e24702a251b255c
Justine Hoang <justinehoang21@gmail.com>
2023-02-14 00:01:25 -0600

    add first solution

---
+++ 5-kyu/valid-parentheses.js
@@ -0,0 +1,22 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// Example: Input: s = "()[]{}" then Output: true
+
+function isValid(s) {
+    let bracket = {
+        '(':')',
+        '[':']',
+        '{':"}"
+    }
+    let heap = [];
+
+    for (let char of s) {
+        if (bracket[char]) {
+            heap.push(bracker[char]);
+        }else {
+            if (heap.pop() !== char) {
+                return false;
+            }
+        }
+    }
+    return (!heap.length);
+}
