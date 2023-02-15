9c10b6224cf39fcf4bce10b0c11dd4f024e2fa1f
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-02-14 23:50:40 -0600

    add updated solution

--- 6-kyu/super-duper-easy.js
+++ 6-kyu/super-duper-easy.js
@@ -1,7 +1,2 @@
 // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
-function problem(x){
-    return typeof x === "number" ? x * 50 + 6 : "Error";
-  }
-
-// OR
 const problem = x => typeof x === 'string' ? 'Error' : x * 50 + 6;
