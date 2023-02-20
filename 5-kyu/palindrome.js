34053a359e8f7d766de792cc71fb2aa263c152c6
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-02-19 23:56:59 -0600

    add updated solution

--- 5-kyu/palindrome.js
+++ 5-kyu/palindrome.js
@@ -1,6 +1,4 @@
 // Given a string s, return true if it is a palindrome, or false otherwise.
-// Note: A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
-
 function isPalindrome(s) {
     let re = /[\W_]/g;
     let lowRegStr = s.toLowerCase().replace(re, '');
