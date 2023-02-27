839619d7893ccc6d21096978e3671eade4d87d3d
Justine Hoang <justinehoang21@gmail.com>
2023-02-26 19:38:43 -0600

    add updated solution

--- 5-kyu/high-and-low.js
+++ 5-kyu/high-and-low.js
@@ -13,3 +13,10 @@ function highAndLow(numbers){
     numbers = numbers.split(" ");
     return Math.max(...numbers) +" "+ Math.min(...numbers);
   }
+
+// MORE EFFICIENT SOLUTION
+
+function highAndLow(numbers){
+  numbers = numbers.split(' ').map(Number);
+  return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
+}
