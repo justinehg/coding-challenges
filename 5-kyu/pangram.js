b443c8f06ccfa845112edfeaa55f00ee426a5b42
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-03-09 22:57:11 -0600

    add first solution

--- 5-kyu/pangram.js
+++ 5-kyu/pangram.js
@@ -13,11 +13,3 @@ const isPangram = (string) => {
     }
     return true;
   }
-//   MORE EFFICIENT SOLUTION
-
-function isPangram(string){
-    string = string.toLowerCase();
-    return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
-      return string.indexOf(x) !== -1;
-    });
-  }
