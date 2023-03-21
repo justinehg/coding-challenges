3f3f4ba35231c6d59b8ac20a767dfb589a6944ab
Justine Hoang <justinehoang21@gmail.com>
2023-03-20 22:16:33 -0500

    add updated solution

--- 5-kyu/list-filtering.js
+++ 5-kyu/list-filtering.js
@@ -3,4 +3,9 @@ function filter_list(l) {
     return l.filter(x => typeof x === 'number');
   }
   console.log(filter_list([1, 'a', 'b', 0, 15]));
-  
+
+//   MORE EFFICIENT SOLUTION.
+
+function filter_list(l) {
+    return l.filter(function(v) {return typeof v == 'number'})
+  }
