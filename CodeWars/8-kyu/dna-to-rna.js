c97930c6250455e51889aaf929d46306364d2e46
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-03-23 23:25:54 -0500

    add updated solution

--- CodeWars/8-kyu/dna-to-rna.js
+++ CodeWars/8-kyu/dna-to-rna.js
@@ -1,5 +1,5 @@
-// Create a function which translates a given DNA string into RNA. For example: "GCAT"  =>  "GCAU"
-// Note: .replace() to replace T with U
+// Create a function which translates a given DNA string into RNA. For example: "GCAT"  =>  "
+
 function DNAtoRNA(dna) {
     return dna.toUpperCase().replace(/T/g, 'U');
   }
