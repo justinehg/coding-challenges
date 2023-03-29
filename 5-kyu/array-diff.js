68bca2f8918977e61d0d266dbf6e7a8b93814ec4
Justine Hoang <justinehoang21@gmail.com>
2023-03-28 21:40:30 -0500

    add updated solution

--- 5-kyu/array-diff.js
+++ 5-kyu/array-diff.js
@@ -2,3 +2,9 @@
 function arrayDiff(a, b) {
     return a.filter(val => !b.includes(val));
 }
+
+// DIFFERENT APPROACH.
+
+function array_diff(a, b) {
+    return a.filter(function(x) { return b.indexOf(x) == -1; });
+  }
