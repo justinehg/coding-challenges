5ef419ea06093c8129df0a57e1e0d71523b9e535
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-03-29 23:10:32 -0500

    add updated solution

--- 5-kyu/array-diff.js
+++ 5-kyu/array-diff.js
@@ -3,7 +3,7 @@ function arrayDiff(a, b) {
     return a.filter(val => !b.includes(val));
 }
 
-// DIFFERENT APPROACH.
+// MORE EFFICIENT SOLUTION
 
 function array_diff(a, b) {
     return a.filter(function(x) { return b.indexOf(x) == -1; });
