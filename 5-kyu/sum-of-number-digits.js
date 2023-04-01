cd006d30a3b7bc51bbb101163616a5a0d0491c33
Justine Hoang <justinehoang21@gmail.com>
2023-03-31 22:39:01 -0500

    add first solution

--- 5-kyu/sum-of-number-digits.js
+++ 5-kyu/sum-of-number-digits.js
@@ -1,3 +1,4 @@
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
 function sumDigits(number) {
     let arr = Math.abs(number).toString().split('')
     let sum = 0;
