5926257be2916f3714f16c20e065fe25026cb4a4
Justine Hoang <justinehoang21@gmail.com>
2023-04-24 22:49:28 -0500

    add first solution

---
+++ 5-kyu/count-characters.js
@@ -0,0 +1,8 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+function count (string) {  
+    var count = {};
+    string.split('').forEach(function(s) {
+       count[s] ? count[s]++ : count[s] = 1;
+    });
+    return count;
+  }
