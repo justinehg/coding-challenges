b213270f0b1fbd24584f4b1c86d50bb468388c38
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-05-01 23:55:50 -0500

    add updated solution

--- 6-kyu/calc-paperwork.js
+++ 6-kyu/calc-paperwork.js
@@ -1,13 +1,4 @@
-// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages
-function paperwork(n, m) {
-    if( n < 0 || m < 0 ) {
-        return 0 
-    } else {
-        return n * m 
-    }
-}
-
-// OR 
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages 
 function paperwork(n, m) {
     return n < 0 || m < 0 ? 0 : n * m; 
 }
