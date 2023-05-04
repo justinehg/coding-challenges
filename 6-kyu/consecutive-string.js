dc3ce9117ba44848351c8eab66d79358309bccd9
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-05-03 23:44:13 -0500

    add updated solution

--- 6-kyu/consecutive-string.js
+++ 6-kyu/consecutive-string.js
@@ -1,22 +1,5 @@
 // You are given an array(list) strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
 function longestConsec(strarr, k) {
-    if (strarr.length == 0 || k > strarr.length || k <= 0) return '';
-    
-    let longStr = '';
-    
-    let newStr = '';
-    
-    for (let i = 0; i < strarr.length; i++){
-      newStr = strarr.slice(i, i+k);
-      if (newStr.join('').length > longStr.length ){
-        longStr = newStr.join('');
-      }
-    }
-    
-    return longStr;
-}
-// OR
-function longestConsec(strarr, k) {
     var longest = "";
     for(var i=0;k>0 && i<=strarr.length-k;i++){
       var tempArray = strarr.slice(i,i+k);
