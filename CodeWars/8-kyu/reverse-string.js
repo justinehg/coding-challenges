983befbcfdecd88cb1055f85c745e9b619776ace
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-05-12 23:22:43 -0500

    add updated solution

--- CodeWars/8-kyu/reverse-string.js
+++ CodeWars/8-kyu/reverse-string.js
@@ -1,4 +1,4 @@
-// Prompt: Write a function that reverse the string
+// Write a function that reverse the string
 function reverseWords(str) {
     return str.split(' ').map(function(word){
       return word.split('').reverse().join('');
