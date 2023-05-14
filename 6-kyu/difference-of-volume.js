980091b5bcb6f550e7d53c857a0b271fbc6773d3
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-05-13 23:59:38 -0500

    Update difference-of-volume.js

--- 6-kyu/difference-of-volume.js
+++ 6-kyu/difference-of-volume.js
@@ -3,10 +3,5 @@ function findDifference(a, b) {
     return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
 }
 
-// OR
-
-function findDifference(a, b) {
-     return Math.abs(a.reduce((a, b) => a * b) - b.reduce((a, b) => a * b))
-}
 
 
