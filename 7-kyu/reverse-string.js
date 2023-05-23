b13ea5aabf2630aac1d682819989a72f46e2f11c
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-05-22 22:29:58 -0500

    add updated solution

--- 7-kyu/reverse-string.js
+++ 7-kyu/reverse-string.js
@@ -1,4 +1,4 @@
-// Complete the solution so that it reverses the string passed into it.
+// Write a solution so that it reverses the string passed into it.
 function solution(str){
     return str.split('').reverse().join('');
   }
