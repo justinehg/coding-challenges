6726dca7c4febe0cf16a40d018be8061f49609f9
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-05-30 16:07:55 -0500

    add first solution

---
+++ 7-kyu/remove-smallest.js
@@ -0,0 +1,9 @@
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+function removeSmallest(numbers) {
+    let indexOfMin = numbers.indexOf(Math.min(...numbers));
+    return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+  }
+
+// ANOTHER APPROACH
+
+const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
