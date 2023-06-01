7a4db562bf681efd0825cd89e09918fe276ac6e6
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-06-01 15:33:03 -0500

    add first solution

---
+++ 5-kyu/equal-sides-array.js
@@ -0,0 +1,16 @@
+// You are going to be given an array of integers. Your job is to take that array and find an index N where the sum of the integers to the left of N is equal to the sum of the integers to the right of N. If there is no index that would make this happen, return -1.
+function findEvenIndex(arr)
+{
+  var left = 0, right = arr.reduce(function(pv, cv) { return pv + cv; }, 0);
+  for(var i = 0; i < arr.length; i++) {
+      if(i > 0) left += arr[i-1];
+      right -= arr[i];
+      
+      if(left == right) return i;
+  }
+  
+  return -1;
+}
+// .MORE EFICIENT SOLUTION
+const sum = (a, from, to) => a.slice(from, to).reduce((a, b) => a + b, 0)
+const findEvenIndex = a => a.findIndex((el, i) => sum(a, 0, i) === sum(a, i + 1));
