92ca71aa2252205e6540d643806ad7b3db0b01d6
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-06-04 23:59:49 -0500

    Update remove-smallest.js

--- 7-kyu/remove-smallest.js
+++ 7-kyu/remove-smallest.js
@@ -4,6 +4,6 @@ function removeSmallest(numbers) {
     return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
   }
 
-// OR.
+// OR
 
 const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
