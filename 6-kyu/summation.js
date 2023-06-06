bf942f4d45651a5986606f33f91711ffc648ecd5
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-06-05 23:55:51 -0500

    add first solution

--- 6-kyu/summation.js
+++ 6-kyu/summation.js
@@ -1,12 +1,4 @@
 // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
-var summation = function(num) {
-    let sum = 0;
-    for (let i = 0; i <= num; i++) {
-        sum += i;
-    }
-    return sum;
-}
-// OR
 var summation = function (num) {
     return num * (num + 1) / 2
 }
