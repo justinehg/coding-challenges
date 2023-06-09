d8e2850b94820eb3a417fe1b30ca7875387a21ad
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-06-08 23:45:32 -0500

    add first solution

--- CodeWars/8-kyu/series-sum.js
+++ CodeWars/8-kyu/series-sum.js
@@ -1,7 +1,7 @@
 // Your task is to write a function which returns the sum of following series upto nth term(parameter).
 // Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
 // Rules: You need to round the answer to 2 decimal places and return it as String.
-// If the given value is 0 then it should return 0.00
+
 function SeriesSum(n) {
     let sum = 0;
     for(let i = 0; i < n; i++) {
