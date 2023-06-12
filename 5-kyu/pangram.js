ae9216d17bcb0a48dd6481fdbff58d40b36d2b4d
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-06-11 23:33:57 -0500

    add updated solution

--- 5-kyu/pangram.js
+++ 5-kyu/pangram.js
@@ -1,19 +1,4 @@
 // Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
-
-const isPangram = (string) => {
-    let alpha = string.toUpperCase().split("");
-    for (let beta = 65; beta < 91; beta++) {
-      let gamma = String.fromCharCode(beta);
-      if (alpha.includes(gamma)) {
-        continue;
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
-//   OR
-
 function isPangram(string){
     string = string.toLowerCase();
     return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
