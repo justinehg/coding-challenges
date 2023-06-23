312e7628ef971bebd1a47b78b375f552415339d2
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-06-22 23:34:11 -0500

    add first solution

--- 6-kyu/sort-orders.js
+++ 6-kyu/sort-orders.js
@@ -1,5 +1,4 @@
-// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
-
+// Sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
 function order(words){
     let arr = words.split(' ')
     let r = []
