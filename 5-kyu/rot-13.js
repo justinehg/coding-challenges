806a168e2fe1c809c043e1587d709333babb603c
Justine Hoang <justinehoang21@gmail.com>
2023-06-23 23:08:04 -0500

    add first solution

---
+++ 5-kyu/rot-13.js
@@ -0,0 +1,6 @@
+// Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+function rot13(message) {
+    var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+    return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
+  }
