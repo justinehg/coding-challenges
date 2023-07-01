b0958d58d60fed5a57be86c5f5f7f2778d00a6da
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-01 00:14:16 -0500

    add updated solution

--- 5-kyu/no-ending-zero.js
+++ 5-kyu/no-ending-zero.js
@@ -1,8 +1,4 @@
-// Numbers ending with zeros are boring. They might be fun in your world, but not here. Get rid of them. Only the ending ones.
-// Examples: 
-// 1450 -> 145
-// 960000 -> 96
-
+// Numbers ending with zeros are boring. They might be fun in your world, but not here. Get rid of them. Only the ending 
 function noBoringZeros(n) {
     while(n%10==0 && n!=0){n/=10;}
     return n;
