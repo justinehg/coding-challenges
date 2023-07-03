c4a77c97ee26f42b0b223133f7bed9612af9aaec
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-02 23:30:58 -0500

    add updated solution

--- CodeWars/8-kyu/filter-case-sensitive.js
+++ CodeWars/8-kyu/filter-case-sensitive.js
@@ -1,4 +1,4 @@
-// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+// Return a string in which each character (case-sensitive) is repeated once.
 function doubleChar(str){
     return str.split("").map(c => c + c).join("");
 }
