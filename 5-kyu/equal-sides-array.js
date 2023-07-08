8d2a014d4068d608b27265ba2c0eba2e24a4e516
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-07 23:07:45 -0500

    add first solution

--- 5-kyu/equal-sides-array.js
+++ 5-kyu/equal-sides-array.js
@@ -11,6 +11,3 @@ function findEvenIndex(arr)
   
   return -1;
 }
-// OR
-const sum = (a, from, to) => a.slice(from, to).reduce((a, b) => a + b, 0)
-const findEvenIndex = a => a.findIndex((el, i) => sum(a, 0, i) === sum(a, i + 1));
