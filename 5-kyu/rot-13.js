a9a2248ec1ad7aa4c69dbed91aef17d6abf9f6b7
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-09 23:26:43 -0500

    add first solution

--- 5-kyu/rot-13.js
+++ 5-kyu/rot-13.js
@@ -5,5 +5,4 @@ function rot13(message) {
     return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
   }
 
-// OR
-const rot13 = str => str.replace(/[a-z]/gi, letter => String.fromCharCode(letter.charCodeAt(0) + (letter.toLowerCase() <= 'm' ? 13: -13)));
+
