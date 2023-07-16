301570b3a66dfc894ba5e8dc47420e92460ec71a
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-15 23:57:57 -0500

    add updated solution

--- 6-kyu/display-texts.js
+++ 6-kyu/display-texts.js
@@ -11,13 +11,13 @@ function likes(names) {
     }
   }
 
-//   OR
-// function likes(names) {
-//     return {
-//       0: 'no one likes this',
-//       1: `${names[0]} likes this`, 
-//       2: `${names[0]} and ${names[1]} like this`, 
-//       3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
-//       4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
-//     }[Math.min(4, names.length)]
-//   }
+OR
+function likes(names) {
+     return {
+       0: 'no one likes this',
+       1: `${names[0]} likes this`, 
+       2: `${names[0]} and ${names[1]} like this`, 
+       3: `${names[0]}, ${names[1]} and ${names[2]} like this`, 
+       4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
+     }[Math.min(4, names.length)]
+}
