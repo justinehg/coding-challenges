4db378aa42ed37b5ecca5511774c0f45d0651a74
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-18 23:43:38 -0500

    add first solution

--- 7-kyu/remove-anchor.js
+++ 7-kyu/remove-anchor.js
@@ -1,4 +1,4 @@
-// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+// Complete the function so that it returns the url with anything after the anchor (#) removed.
 function removeUrlAnchor(url){
     return url.split('#')[0];
   }
