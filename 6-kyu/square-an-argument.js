4d83e1e8f8346780ea012761d91b8614198ce63e
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-22 00:00:29 -0500

    add first solution

--- 6-kyu/square-an-argument.js
+++ 6-kyu/square-an-argument.js
@@ -1,2 +1,2 @@
-// Now you have to write a function that takes an argument and returns the square of it.
+// Write a function that takes an argument and returns the square of it.
 const square = (n) => n * n;
