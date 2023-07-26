de027590f1f8888eb5f315aaa8a1558321f95415
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-25 23:37:00 -0500

    add first solution

--- 6-kyu/pipelines.js
+++ 6-kyu/pipelines.js
@@ -1,4 +1,4 @@
-// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+// Return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
 function fixPipes(numbers) {
     let min = Math.min.apply(this, numbers);
     let max = Math.max.apply(this, numbers);
