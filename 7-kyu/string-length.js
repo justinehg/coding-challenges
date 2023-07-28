18630c4923d3bd8378de67e1432c4dd377ac0257
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-27 23:52:41 -0500

    add first solution

--- 7-kyu/string-length.js
+++ 7-kyu/string-length.js
@@ -1,4 +1,4 @@
-// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+// The solution should returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
 function solution(str, ending){
     return str.indexOf(ending, str.length - ending.length) !== -1;
   }
