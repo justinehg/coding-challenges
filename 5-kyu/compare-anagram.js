d37260b2c5c2aad1e9c26fc5dc31ed1f2fa44dec
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-07-28 23:20:39 -0500

    add first solution

--- 5-kyu/compare-anagram.js
+++ 5-kyu/compare-anagram.js
@@ -1,4 +1,4 @@
-// Complete the function to return true if the two arguments given are anagrams of each other; return false 
+// Write a function to return true if the two arguments given are anagrams of each other; return false 
 String.prototype.sortLetters = function() {
     return this.toLowerCase().split('').sort().join('');
   }
