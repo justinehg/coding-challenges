b712a26284feabfb1d8d9ade8778f84f7cdb3dfe
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-08-01 23:18:33 -0500

    add first solutio

--- CodeWars/8-kyu/determine-age.js
+++ CodeWars/8-kyu/determine-age.js
@@ -1,4 +1,4 @@
-// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9. Write a program that returns the girl's age (0-9) as an integer.
+// When you ask someone,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9. Write a program that returns the girl's age (0-9) as an integer.
 
 function getAge(inputString){
     return parseInt(inputString);
