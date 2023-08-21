ee715542205387858741da9249b751337747b814
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-08-20 23:55:44 -0500

    add first solution

--- 7-kyu/remove-them-vowels.js
+++ 7-kyu/remove-them-vowels.js
@@ -1,4 +1,4 @@
-// Write a function that takes a string and return a new string with all vowels removed. For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+// Create a function that takes a string and return a new string with all vowels removed. For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 function disemvowel(str) {
     return str.replace(/[aeiou]/gi, '');
   }
