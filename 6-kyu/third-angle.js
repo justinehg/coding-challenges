598725eaa0826d26606c84cd01748f67ae0a6bf2
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-08-21 23:51:50 -0500

    add first solution

--- 6-kyu/third-angle.js
+++ 6-kyu/third-angle.js
@@ -1,5 +1,4 @@
-// You are given two interior angles (in degrees) of a triangle. Write a function to return the 3rd.
-
+// Given two interior angles (in degrees) of a triangle. Write a function to return the 3rd.
 function otherAngle(a,b) {
     return 180 - (a + b);
 }
