5cba69d0d09df75e0c0da3f49a2a72f3d18b2254
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-09-03 21:36:06 -0500

    Update 3.0-puzzle-solver.js
    This particular solution is being used with the Breadth-first search approach. This is the most straightforward method yet it will requires more running time. In the process of testing out A*

--- 5-kyu/3.0-puzzle-solver.js
+++ 5-kyu/3.0-puzzle-solver.js
@@ -1,4 +1,4 @@
-// Solution using Breadth-first search. With HOT URL and API endpoints, need to replace HOST URL with actual HOST NAME
+// Fin solution using Breadth-first search. With HOT URL and API endpoints, need to replace HOST URL with actual HOST NAME
 const https = require('https');
 
 const HOST_URL = 'https://code-test-sliding-squares-rrskuhktnq-uc.a.run.app';
