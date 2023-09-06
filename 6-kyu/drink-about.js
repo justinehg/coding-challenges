d8509585286ba63ec7842c8f11e6cc9cf795973e
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-09-05 23:25:15 -0500

    add first solution

--- 6-kyu/drink-about.js
+++ 6-kyu/drink-about.js
@@ -1,5 +1,4 @@
-// Make a function that receive age, and return what they drink. Children under 14 old. Teens under 18 old. Young under 21 old. Adults have 21 or more.
-
+// Create a function that receive age, and return what they drink. Children under 14 old. Teens under 18 old. Young under 21 old. Adults have 21 or more.
 const peopleWithAgeDrink = (age) =>
   age < 14 ? "drink toddy" :
   age < 18 ? "drink coke" :
