add1d8134ff8222db1694868f541aa57e6f60814
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-09-09 23:56:23 -0500

    add first solution

--- 5-kyu/palindrome.js
+++ 5-kyu/palindrome.js
@@ -1,4 +1,4 @@
-// Given a string s, return true if it is a palindrome, or false otherwise.
+// Given a string, return true if it is a palindrome, or false otherwise.
 function isPalindrome(s) {
     let re = /[\W_]/g;
     let lowRegStr = s.toLowerCase().replace(re, '');
