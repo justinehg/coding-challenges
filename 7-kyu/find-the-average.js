2686e69cd82ecc426ee511b1078b904893d18918
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-09-12 23:58:23 -0500

    add first solution

--- 7-kyu/find-the-average.js
+++ 7-kyu/find-the-average.js
@@ -1,4 +1,4 @@
-// Write a function which calculates the average of the numbers in a given list. Note: Empty arrays should return 0.
+// Create a function which calculates the average of the numbers in a given list. Note: Empty arrays should return 0.
 function find_average(arr) {
     return arr.length > 0 ? arr.reduce((a, b) => a + b) / arr.length : 0;
 }
