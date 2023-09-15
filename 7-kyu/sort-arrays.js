3ca9e5b01cb3523d568e215550b9b2077569390b
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-09-14 23:56:44 -0500

    add first solution

--- 7-kyu/sort-arrays.js
+++ 7-kyu/sort-arrays.js
@@ -1,4 +1,4 @@
-// Write the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+// Provide a solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
 function solution(nums){
     return nums == null ? [] : nums.sort((a,b)=> a-b)
   }
