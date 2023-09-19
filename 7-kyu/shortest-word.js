f1b79d47811c5144d41cb41d3856e9772f270e87
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-09-18 23:25:20 -0500

    add first solution

--- 7-kyu/shortest-word.js
+++ 7-kyu/shortest-word.js
@@ -1,4 +1,4 @@
-// Simple, given a string of words, return the length of the shortest word(s). String will never be empty and you do not need to account for different data types.
+// Given a string of words, return the length of the shortest word(s). String will never be empty and you do not need to account for different data types.
 function findShort(s){
     return Math.min(...s.split(" ").map (s => s.length));
 }
