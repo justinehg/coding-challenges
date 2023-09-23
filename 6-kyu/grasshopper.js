021458b25e2452d38c8f4bc246c66722cb40579f
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-09-22 23:35:45 -0500

    add first solution

--- 6-kyu/grasshopper.js
+++ 6-kyu/grasshopper.js
@@ -1,2 +1,2 @@
-// In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times. Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+// The hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times. Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
 const move = (position, roll) => position + roll * 2
