aa7a879dfca849491bd1fa5dfd86560f15d1f38e
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-09-26 23:13:04 -0500

    add updated solution

--- 6-kyu/num-to-str.js
+++ 6-kyu/num-to-str.js
@@ -1,4 +1,4 @@
-// Write a function that can transform a number (integer) into a string.
+// Create a function that can transform a number (integer) into a string.
 function numberToString(num) {
     return num.toString();
   }
