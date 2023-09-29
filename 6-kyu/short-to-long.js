36a847dd0e0949daf0e7427f304e8bd1d117236a
Justine Hoang <101066358+justinehg@users.noreply.github.com>
2023-09-29 15:18:24 -0500

    add first solution

--- 6-kyu/short-to-long.js
+++ 6-kyu/short-to-long.js
@@ -1,4 +1,4 @@
-// Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest
+// Create a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest
 function sortByLength (array) {
     return array.sort((a,b) => a.length - b.length);
   };
